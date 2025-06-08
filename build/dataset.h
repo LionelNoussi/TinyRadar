ead1b531e933077eb4d7d231aa83ddf2160e56f3
Lionel Noussi <lio.noussi@gmail.com>
2025-06-08 12:11:37 +0200

    Added build directory to repo

---
+++ build/dataset.h
@@ -0,0 +1,3 @@
+#ifndef DATASET_H
+#define DATASET_H
+
---
+++ build/my_model.h
@@ -0,0 +1,7 @@
+#ifndef C_MODEL_H
+#define C_MODEL_H
+
+extern const unsigned char my_model[];
+extern const unsigned int my_model_len;
+
+#endif
